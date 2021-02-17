c409bec34deba257894a59ce70e31463a6faec46
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2021-02-17 17:19:04 +0300

    form

---
+++ src/blocks/modules/profile/box-bill/box-bill.js
@@ -0,0 +1,13 @@
+modules.define('box-bill', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
